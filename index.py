06e8ae4621ca1efb57c740d4f2e1c2be3863f983
Harshi513 <harimarrapu2005@gmail.com>
2024-08-07 21:00:24 +0530

    opening a file in read mode

--- index.py
+++ index.py
@@ -7,3 +7,12 @@ import pyttsx3
 
 # Initialize text-to-speech engine
 engine = pyttsx3.init()
+
+
+# opening the file in read mode
+my_file = open("C:\\Users\\harim\\OneDrive\\Desktop\\coco.txt", "r")
+# reading the file
+data = my_file.read()
+# replacing end splitting the text | when newline ('\n') is seen.
+class_list = data.split("\n")
+my_file.close()
