425bb1d13be455eca1537b009c70da6761ad2235
Harshi513 <harimarrapu2005@gmail.com>
2024-08-07 21:03:20 +0530

    loading pretrained YOLOv8n model

--- index.py
+++ index.py
@@ -25,3 +25,21 @@ for i in range(len(class_list)):
     g = random.randint(0,255)
     b = random.randint(0,255)
     detection_colors.append((b,g,r))
+
+# load a pretrained YOLOv8n model
+model = YOLO("weights/yolov8n.pt", "v8") 
+
+# Vals to resize video frames | small frame optimise the run 
+frame_wid = 640
+frame_hyt = 480
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+start_time = time.time()
+frame_count = 0
+current_confidence = 0.45  # Initial confidence threshold
+confidence_step = 0.05     # Step for adjusting the confidence threshold
